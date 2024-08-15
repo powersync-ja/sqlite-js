a9b1e3203b1867dc9452ba52589e3ee418a2b849
Ralf Kistner <ralf@journeyapps.com>
2024-08-15 18:21:46 +0200

    Cleanup.

--- packages/sqlite-js-driver/src/sqlite-error.ts
+++ packages/sqlite-js-driver/src/sqlite-error.ts
@@ -18,16 +18,3 @@ export class SqliteError extends Error {
     }
   }
 }
-
-export function mapError(error: unknown): SqliteError {
-  const e = error as any;
-  let code = e.code ?? 'SQLITE_ERROR';
-  if (code == 'ERR_SQLITE_ERROR') {
-    code = 'SQLITE_ERROR';
-  }
-  return new SqliteError({
-    code,
-    message: e.message!,
-    stack: e.stack
-  });
-}
